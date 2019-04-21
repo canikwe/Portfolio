8a53576883c1c4ede78c3dba104691b2765d0cad
canikwe <c.anikwe@gmail.com>
2019-04-21 18:25:32 -0400

    Updates

---
+++ precache-manifest.0800882f77ac97f5d894b7374f0b9e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a6fd4a31458480bbb88",
+    "url": "/Portfolio/static/css/main.2433d3dc.chunk.css"
+  },
+  {
+    "revision": "8a6fd4a31458480bbb88",
+    "url": "/Portfolio/static/js/main.356a4f26.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Portfolio/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1d2960ea5585c0150a7699ea2c073b04",
+    "url": "/Portfolio/index.html"
+  }
+];
