447f7421c2f31628de12e03815e88b9e5f1c6ba2
Chine Anikwe <c.anikwe@gmail.com>
2022-10-15 15:52:02 -0400

    remove unused logo and rename test file to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Home from './Home/Home'
 import About from './About/About'
 import ProjectsList from './Projects/ProjectsList'
-import './scss/App.scss'
 import BlogsList from './Blogs/BlogsList'
 import Contact from './Contact/Contact'
 import Footer from './Footer/Footer'
 import NavBar from './Home/NavBar'
+import './scss/App.scss'
 
 const App = () => {
   return (
